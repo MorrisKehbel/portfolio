aa9d007a69717b3c9fc1f4c6a506015bb5a341ea
MorrisKehbel <morris.kehbel@gmail.com>
2025-09-04 18:01:58 +0200

    added language for about and some changes for mobile layout

--- src/components/wrapper/AnimatedText.tsx
+++ src/components/wrapper/AnimatedText.tsx
@@ -18,9 +18,9 @@ export const AnimatedText = ({
       <motion.p
         key={id}
         className={className}
-        initial={{ opacity: 0, y: 0 }}
+        initial={{ opacity: 0, y: 5 }}
         animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 0 }}
+        exit={{ opacity: 0, y: -5 }}
         transition={{ duration: 0.3, ease: "easeInOut" }}
       >
         {children}
