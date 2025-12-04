9e5246a25939acf3e8458d8fb8feb573aa84f66a
MorrisKehbel <morris.kehbel@gmail.com>
2025-12-04 19:04:05 +0100

    updated next.js and react.js versions and small styling changes

--- src/components/portfolio/About.tsx
+++ src/components/portfolio/About.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-
 import { useLanguage } from "@/context/LanguageContext";
 import { AnimatedText } from "@/components/wrapper/AnimatedText";
 
